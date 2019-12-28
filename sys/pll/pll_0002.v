7df07ef4262f5f732f80a02040b14176b3889cd5
MrX-8B <misterx6809@gmail.com>
2019-12-28 16:44:41 +0900

    Fix HDMI clock

--- sys/pll/pll_0002.v
+++ sys/pll/pll_0002.v
@@ -25,7 +25,7 @@ module  pll_0002(
 		.output_clock_frequency0("14.318181 MHz"),
 		.phase_shift0("0 ps"),
 		.duty_cycle0(50),
-		.output_clock_frequency1("28.636363 MHz"),
+		.output_clock_frequency1("57.272727 MHz"),
 		.phase_shift1("0 ps"),
 		.duty_cycle1(50),
 		.output_clock_frequency2("0 MHz"),
